439f58f0ba7717fa6539a647265f534b69d53566
Eder Nilson <eder.nilson@gmail.com>
2026-05-04 08:42:51 -0300

    fix(newsnotfound): fix error param property

--- src/components/ui/NewsNotFound.tsx
+++ src/components/ui/NewsNotFound.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 interface NewsNotFoundProps {
-  query: string;
+  query: string | undefined;
 }
 
 export default function NewsNotFound({ query }: NewsNotFoundProps) {
